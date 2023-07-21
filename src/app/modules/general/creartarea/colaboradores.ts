ceccdfbe65a274c5e3a149977496e0c116bc6761
Marcos Estrella <marcosestrella9@gmail.com>
2023-07-21 17:24:38 -0500

    cambios

--- src/app/modules/general/creartarea/colaboradores.ts
+++ src/app/modules/general/creartarea/colaboradores.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import { NodeService } from "../nodeservice";
-import { Router } from '@angular/router';
+import {NodeService} from "../nodeservice";
+import {Router} from '@angular/router';
 
 @Component({
     selector:'app-colaboradores',
@@ -12,6 +12,7 @@ export class ColaboradoresComponent implements OnInit, OnChanges {
     colaboradores: any[];
     isShowModalColaborators:boolean=false;
     selectedColaborators=[];
+    selectedColaboratorsTable=[];
     @Input()
     dosave = false;
     constructor(public nodeService: NodeService, private router: Router) { this.loadListas(); }
@@ -47,9 +48,16 @@ export class ColaboradoresComponent implements OnInit, OnChanges {
     AddColaborators(){
         this.closeViewColaborators();
         this.selectedColaborators=this.nodeService.informacionTarea.colaboradores;
+        this.selectedColaboratorsTable=this.selectedColaborators;
+        this.selectedColaboratorsTable.forEach(col => {
+            col.check = false;
+        });
     }
 
     toggleColItem(col){
-
+        col.check = !col.check;
+        this.nodeService.informacionTarea.colaboradores = this.selectedColaboratorsTable.filter(col => {
+            return col.check
+        });
     }
 }
